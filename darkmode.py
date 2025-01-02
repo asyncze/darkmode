746454a3673661dc7de9095b9652421feb14aec4
Michael Sjöberg <mail@michaelsjoeberg.com>
2025-01-02 16:19:57 +0700

    Update darkmode.py

--- darkmode.py
+++ darkmode.py
@@ -10,9 +10,9 @@
 import rumps
 import os
 
-class DarkMode(rumps.App):
+class Darkmode(rumps.App):
     @rumps.clicked("Toggle Dark Mode")
     def toggle_dark_mode(self, _): os.system("""open -a 'ToggleDarkMode'""")
 
-if __name__ == "__main__": DarkMode("Dark Mode").run()
+if __name__ == "__main__": DarkMode("Darkmode").run()
 
